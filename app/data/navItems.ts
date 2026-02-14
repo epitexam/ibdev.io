606bb3daba2d039dabd1167f33bc4a37e39fc473
Epitexam <202238868+epitexam@users.noreply.github.com>
2026-02-14 11:05:09 +0100

    Adding new link to navitems

--- app/data/navItems.ts
+++ app/data/navItems.ts
@@ -4,6 +4,8 @@ import type { NavItem } from "~/components/molecules/Navmenu";
 export const NAV_ITEMS: NavItem[] = [
     { label: "About", href: "#about" },
     { label: "Skills", href: "#skills" },
+    { label: "Experiences", href: "#experience" },
+    { label: "Contact", href: "#contact" },
     { label: "Blog", href: "https://epitexam.github.io/" },
 ];
 
