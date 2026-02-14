b9081987bf089c0090f438e12e420b905dabccaa
Epitexam <202238868+epitexam@users.noreply.github.com>
2026-02-14 09:29:06 +0100

    feat(molecules): enhancing responsiveness and style

--- app/components/molecules/SocialLinks.tsx
+++ app/components/molecules/SocialLinks.tsx
@@ -1,6 +1,5 @@
 import type { SocialLink } from "~/types/contact";
-import { SocialLinkButton } from "./SocialLinkButton";
-
+import { SocialLinkButton } from "../atoms/SocialLinkButton";
 
 interface SocialLinksProps {
     links: SocialLink[];
@@ -8,7 +7,7 @@ interface SocialLinksProps {
 
 export function SocialLinks({ links }: SocialLinksProps) {
     return (
-        <div className="grid grid-cols-1 gap-3">
+        <div className="grid grid-cols-2 lg:grid-cols-1 gap-3 w-full">
             {links.map((link) => (
                 <SocialLinkButton
                     key={link.id}
